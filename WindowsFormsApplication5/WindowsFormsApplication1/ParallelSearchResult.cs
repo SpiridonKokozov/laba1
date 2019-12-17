d5d10f3bfa7a3853ba5f0f264e2a11d7bc09b51c
SpiridonKokozov <55084609+SpiridonKokozov@users.noreply.github.com>
2019-12-17 16:16:42 +0300

    new commit

---
+++ WindowsFormsApplication5/WindowsFormsApplication1/ParallelSearchResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class ParallelSearchResult
+    {
+        /// <summary>
+        /// Найденное слово
+        /// </summary>
+        public string word { get; set; }
+        /// <summary>
+        /// Расстояние
+        /// </summary>
+        public int dist { get; set; }
+        /// <summary>
+        /// Номер потока
+        /// </summary>
+        public int ThreadNum { get; set; }
+    }
+}
